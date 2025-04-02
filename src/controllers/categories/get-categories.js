db0eeed8e54dbd0ca84a25f5639e121e9e131afc
enhbayasgalan <enhuush0109@gmail.com>
2025-04-02 20:29:18 +0800

    sssww

--- src/controllers/categories/get-categories.js
+++ src/controllers/categories/get-categories.js
@@ -1,11 +1,29 @@
-import { Category } from "../../schemas/categories.schema.js"
+import { Category } from "../../schemas/categories.schema.js";
 
 export const getCategories = async (req, res) => {
-    try {
-        const categories = await Category.find()        
-        res.status(200).send(categories)
-    } catch (error) {
-        res.status(500).send("Not Fail")
-        console.log(error)
-    }
-}
+  try {
+    const categories = await Category.aggregate([
+      {
+        $match: {},
+      },
+      {
+        $lookup: {
+          from: "foods",
+          localField: "_id",
+          foreignField: "category",
+          as: "foods",
+        },
+      },
+      {
+        $project: {
+          categoryName: 1,
+          food_count: { $size: "$foods" },
+        },
+      },
+    ]);
+    res.status(200).send(categories);
+  } catch (error) {
+    res.status(500).send("Not Fail");
+    console.log(error);
+  }
+};
--- src/controllers/login/post-login.js
+++ src/controllers/login/post-login.js
@@ -1,35 +1,37 @@
 import { json } from "express";
 import { Users } from "../../schemas/users.schema.js";
 import bcrypt from "bcrypt";
-import jsonwebtoken from 'jsonwebtoken'
+import jsonwebtoken from "jsonwebtoken";
 
 export const checkpassword = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await Users.findOne({email: email});
+    const user = await Users.findOne({ email: email });
 
     if (!user) {
-         res.send("user not found").status(400);
+      res.send("user not found").status(400);
     }
     console.log(user);
 
     const passwordCorrect = await bcrypt.compare(password, user.password);
 
     console.log(passwordCorrect);
-    
-    if (!passwordCorrect) {
-       res.send("Wrong password").status(400)
-    }else{
 
-    console.log(process.env.JWT_TOKEN_SECRET_KEY);
-    
-    const token =  jsonwebtoken.sign({userId:user._id}, process.env.JWT_TOKEN_SECRET_KEY,)
-    console.log(token);
-    
-    res.send(token).status(200)
+    if (!passwordCorrect) {
+      res.send("Wrong password").status(400);
+    } else {
+      console.log(process.env.JWT_TOKEN_SECRET_KEY);
+
+      const token = jsonwebtoken.sign(
+        { email: user.email, role: user.role, address: user.address },
+        process.env.JWT_TOKEN_SECRET_KEY
+      );
+      console.log(token);
+
+      res.send(token).status(200);
     }
   } catch (error) {
     console.error(error);
-     res.send(500).json({message: "interval server error"})
+    res.send(500).json({ message: "interval server error" });
   }
 };
